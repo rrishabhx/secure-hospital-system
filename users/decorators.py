9b20d7b3c6f45929f6f26761acc5de8b944648b3
risabhrizz <rraj21@asu.edu>
2022-03-10 12:11:48 -0700

    Create user authorization decorators

---
+++ users/decorators.py
@@ -0,0 +1,92 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
+
+
+def patient_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is a patient,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'patient',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def hospital_staff_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is a hospital-staff,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'hospital_staff',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def doctor_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is a doctor,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'doctor',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def insurance_staff_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is an insurance-staff,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'insurance_staff',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def lab_staff_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is a lab-staff,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'lab_staff',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def administrator_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    """
+    Decorator for views that checks that the logged-in user is an administrator,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.user_type == 'administrator',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator

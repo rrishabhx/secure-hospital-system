c2d0a97d70ca9126ad2279e902670334f416c43b
anantvatta <anantvatta@outlook.com>
2022-03-11 14:58:06 -0700

    Revert "Update views.py"
    This reverts commit 265f5d764830be9ac885c1538c94ca3fd05d6a6d.

--- administrators/views.py
+++ administrators/views.py
@@ -25,7 +25,6 @@ def home(request):
 
     empData = Employee.objects.all()
     fnEmpData = Employee.objects.values('first_name', 'last_name')
-    form = CreateEmployeeForm()
     context = {
         'empData': empData,
         'form': form,

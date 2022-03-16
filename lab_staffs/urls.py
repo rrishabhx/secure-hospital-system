fea76f3c19a6e333f4e4ad247544fa0e80ca9df1
JianLin <jlin74@asu.edu>
2022-03-16 15:43:13 -0700

    updating labstaff functions

--- lab_staffs/urls.py
+++ lab_staffs/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='labstaff-home'),
+    path('/viewdiagnosis', views.viewDiagnosis, name='viewDiagnosis'),
+    path('/createreport', views.createReport, name='createReport'),
+    path('/deletereport', views.deleteReport, name='deleteReport'),
+    path('/updatereport', views.updateReport, name='updateReport'),
 ]
--- lab_staffs/views.py
+++ lab_staffs/views.py
@@ -4,4 +4,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Lab staff's Home page</h1>")
+    context = {}
+
+    return render(request, "lab_staffs/labstaff-home.html", context)
+
+def viewDiagnosis(request, patientID):
+    return render(request, "lab_staffs/viewDiagnosis.html", context)
+
+def createReport(request, patientID, authorization):
+    return render(request, "lab_staffs/createReport.html", context)
+
+def deleteReport(request, patientID):
+    if ():
+        return HttpResponse("deleted")
+    else:
+        return HttpResponse("deleted failed")
+
+def updateReport(request, patientID):
+    return render(request, "lab_staffs/updateReport.html", context)
+
+def approveTest(request, patientID, authorization, testType):
+    if ():
+        return HttpResponse("approve")
+    else:
+        return HttpResponse("approve failed")

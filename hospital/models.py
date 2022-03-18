5481f7ecc2be15b5ffccbc44b34e189f3442bd1f
risabhrizz <rraj21@asu.edu>
2022-03-18 11:10:13 -0700

    Create hospital functionality models

--- hospital/models.py
+++ hospital/models.py
@@ -3,17 +3,56 @@ from django.db import models
 from patients.models import PatientProfile
 from doctors.models import DoctorProfile
 
+'''
+Many-to-one relationship example:
+if a Car model has a Manufacturer – that is, a Manufacturer makes multiple cars but each Car only has one Manufacturer
 
+from django.db import models
+
+class Manufacturer(models.Model):
+    # ...
+    pass
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    # ...
+
+'''
+
+
+# TODO: Review doctor availability, Transactions, Insurance functionality
 class Appointment(models.Model):
+    """
+    Appointment requested: status=False
+    Appointment approved: status=True
+    """
     patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
     doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(null=True, blank=True)
     scheduled_date = models.DateTimeField()
-    appointment_details = models.CharField(max_length=100)
+    appointment_details = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True, blank=True)
 
     class Meta:
         ordering = ['-scheduled_date', '-created']
 
     def __str__(self):
-        return f"P:{self.patient},D:{self.doctor},Apt:{self.appointment_details}"
+        return f"{self.patient},{self.doctor},{self.appointment_details}"
+
+
+class Diagnosis(models.Model):
+    appointment = models.OneToOneField(Appointment, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    details = models.TextField(null=True, blank=True)
+    prescription = models.TextField(null=True, blank=True)
+    lab_test_recommended = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class LabTest(models.Model):
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE)
+    test_type = models.CharField(max_length=150)
+    lab_test_report = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
--- patients/views.py
+++ patients/views.py
@@ -3,7 +3,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 
 from hospital.forms import AppointmentCreationForm
-from hospital.models import Appointment
+from hospital.models import Appointment, Diagnosis, LabTest
 from patients.models import PatientProfile
 from users.decorators import patient_required
 from django.shortcuts import render, get_object_or_404
@@ -17,12 +17,17 @@ logger = logging.getLogger(__name__)
 @login_required
 @patient_required
 def home(request):
-    user = get_object_or_404(PatientProfile, pk=request.user.pk)
-    logger.info(f"Inside home of {user}")
+    logger.info(f"Inside home of {request.user}")
+
+    user = get_object_or_404(PatientProfile, user=request.user)
+    logger.info(f"User object: {user}")
+
     appointment_form = AppointmentCreationForm()
 
     context = {
         'appointments': Appointment.objects.filter(patient=user),
+        'diagnosis': Diagnosis.objects.filter(patient=user),
+        'lab_tests': LabTest.objects.filter(patient=user),
         'appointment_form': appointment_form
     }
 

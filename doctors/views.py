3ce539829263a1c8dd1c3581e8f8a24bbeb671f9
Yahsaswi <ypochamp@asu.edu>
2022-03-18 16:38:38 -0700

    Initial UI of doctors

--- doctors/views.py
+++ doctors/views.py
@@ -1,7 +1,33 @@
+
+
 from django.http import HttpResponse
 from django.shortcuts import render
+import logging
+
+from django.contrib.auth.decorators import login_required
+
+from hospital.forms import AppointmentCreationForm
+from hospital.models import Appointment, Diagnosis, LabTest
+from doctors.models import DoctorProfile
+from users.decorators import doctor_required
+from django.shortcuts import render, get_object_or_404
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
+@login_required
+@doctor_required
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Doctor's Home page</h1>")
+    logger.info(f"Inside home of {request.user}")
+    user = get_object_or_404(DoctorProfile, user=request.user)
+    logger.info(f"User object: {user}")
+    context = {
+        'appointments': Appointment.objects.filter(doctor=user),
+        'diagnosis': Diagnosis.objects.filter(doctor=user),
+        'lab_tests': LabTest.objects.filter(doctor=user),
+    }
+    return render(request, 'doctors/home.html', context)

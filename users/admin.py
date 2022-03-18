dfaf94af55cb9d707dac952726c45ac8ecc35e53
risabhrizz <rraj21@asu.edu>
2022-03-18 09:56:22 -0700

    Update Admin page for user models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,62 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+class UserCreationForm(forms.ModelForm):
+    USER_TYPE_CHOICES = (
+        ('patient', 'Patient'),
+        ('hospital_staff', 'Hospital Staff'),
+        ('doctor', 'Doctor'),
+        ('insurance_staff', 'Insurance Staff'),
+        ('lab_staff', 'Lab Staff'),
+        ('administrator', 'Administrator'),
+    )
+
+    user_type = forms.Select(choices=USER_TYPE_CHOICES)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'user_type', 'password1', 'password2')}
+         ),
+    )
+
+    fieldsets = (
+        *BaseUserAdmin.fieldsets,  # original form fieldsets, expanded
+        (  # new fieldset added on to the bottom
+            'Custom User Fields',  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'user_type', 'date_of_birth'
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)

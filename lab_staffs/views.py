b3a789837755925e7fb0f453c329288a267ec215
JianLin <jlin74@asu.edu>
2022-03-25 16:47:33 -0700

    add new form

--- lab_staffs/views.py
+++ lab_staffs/views.py
@@ -1,24 +1,45 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_list_or_404
-from patients.models import PatientRecords
+from users.models import User
+import logging
+from django.shortcuts import render
+from users.decorators import lab_staff_required
+from hospital.models import Diagnosis, LabTest
 
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home(request):
-    patientRecord = PatientRecords.objects.all()
-    context = {'patientRecords':patientRecord}
+    logger.info(f"Inside home of {request.user}")
+    user = request.user.labstaffprofile
+    logger.info(f"User object: {user}")
+
+    context = {'staffFile':user}
     return render(request, "lab_staffs/labstaff-home.html", context)
 
 def viewDiagnosis(request):
-    if 'patientID' in request.GET and request.GET['patientID']:
-        patientID = request.GET['patientID']
-        patientRecord = get_list_or_404(PatientRecords, id = patientID)
-        context = {"patientRecord":patientRecord}
-    else:
-        context= {'Error':'Error, empty patient ID'}
+    if request.method == "POST":
+        # if 'username' in request.POST and request.POST['username']:
+        #     username = request.POST['username']
+        #     user = User.objects.get(username=username)
+        #     patientProfile = Diagnosis.objects.get(user=user)
+        #     patientDiagnosis_list = get_list_or_404(Diagnosis, patient=patientProfile)
+        #     context = {"patientDiagnosis": patientDiagnosis_list}
+        # else:
+        #     context = {'Error': 'Error, empty patient name'}
+        diagnosis = Diagnosis.objects.all()
+
     return render(request, "lab_staffs/viewDiagnosis.html", context)
 
 def createReport(request, patientID, authorization):
+    if request.method == "POST":
+        if 'patient_name' in request.POST and request.POST['patient_name']:
+            username = request.POST['patient_name']
+            user = User.objects.get(username=username)
+            patientProfile = PatientProfile.objects.get(user=user)
+            patientDiagnosis_list = get_list_or_404(Diagnosis, patient=patientProfile)
+            context = {"patientDiagnosis": patientDiagnosis_list}
+        else:
+            context = {'Error': 'Error, empty patient name'}
     context = {}
     return render(request, "lab_staffs/createReport.html", context)
 
@@ -32,8 +53,15 @@ def updateReport(request, patientID):
     context = {}
     return render(request, "lab_staffs/updateReport.html", context)
 
-def approveTest(request, patientID, authorization, testType):
-    if ():
-        return HttpResponse("approve")
-    else:
-        return HttpResponse("approve failed")
+def approveTest(request):
+    if request.method == "POST":
+        if 'username' in request.POST and request.POST['username']:
+            username = request.POST['username']
+            user = User.objects.get(username=username)
+            patientProfile = PatientProfile.objects.get(user=user)
+            patientDiagnosis_list = get_list_or_404(Diagnosis, patient=patientProfile)
+            context = {"patientDiagnosis": patientDiagnosis_list}
+        else:
+            context = {'Error': 'Error, empty patient name'}
+
+    return render(request, "lab_staffs/approveTest.html", context)

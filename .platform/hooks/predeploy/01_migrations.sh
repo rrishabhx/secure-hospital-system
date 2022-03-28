c9e9e243897f75279cb6cd0b7222ffb56e5f11d2
risabhrizz <rraj21@asu.edu>
2022-03-27 20:22:59 -0700

    Add platform hooks for eb deployment

---
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /var/app/venv/*/bin/activate
+cd /var/app/staging
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createfirstsuperuser
+python manage.py collectstatic --noinput

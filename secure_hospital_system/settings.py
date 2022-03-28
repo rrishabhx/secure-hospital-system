341bb46c908aa45e578a4f595969345c68b47716
risabhrizz <rraj21@asu.edu>
2022-03-27 21:12:31 -0700

    Add Amazon S3 config for eb deployment

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'bootstrap5',
     'django_bootstrap_icons',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -165,6 +166,17 @@ TIME_ZONE = 'US/Arizona'
 USE_I18N = True
 
 USE_TZ = True
+
+# Amazon S3 configuration
+if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+
+    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/

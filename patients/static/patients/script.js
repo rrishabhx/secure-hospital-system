7b38c51fb97c6683114451582a5582262be4ce9a
Risabh Raj <ratedrrisabh@gmail.com>
2022-03-29 18:41:54 -0700

    Revert "Initial chatbot integration"

--- patients/static/patients/script.js
+++ patients/static/patients/script.js
@@ -7,8 +7,3 @@ function newAppointment(){
     var object = document.getElementById('new-appointment-block');
     object.classList.toggle('d-none');
 }
-function popitup(url) {
-    newwindow=window.open(url,'{{title}}','height=200,width=150');
-    if (window.focus) {newwindow.focus()}
-    return false;
-}

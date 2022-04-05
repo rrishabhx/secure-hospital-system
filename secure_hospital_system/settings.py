229ef4897292fab5f8b95f89a577b0f537cad9b6
Risabh Raj <rraj21@asu.edu>
2022-04-05 10:39:25 -0700

    Update settings.py

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-4lz@t)(@na8t$p@4egmxqd9fb#z=1*6k=5(yhyl0t(z$*zh0a$")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rztestapp.com', 'secure-hospital-system-env.us-west-1.elasticbeanstalk.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rztestapp.com', 'www.rztestapp.com']
 
 # Application definition
 
